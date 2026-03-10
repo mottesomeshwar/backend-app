af1bed09a5684cfbcbe8533bc89d03dbaba445be
Someshwar Patel <someshwarpatelm@gmail.com>
2026-03-10 13:39:14 +0530

    updated

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
 import dotenv from "dotenv";
-// import {productRouter} from "./routes/productRoute.js";
-import { storeRouter } from "./routes/storeRoute.js";
+import mongoose from "mongoose";
 import dbConnect from "./config/db.js";
+import productRouter from "./routes/productRoute.js";
+import { storeRouter } from "./routes/storeRoute.js";
 const app = express();
 dotenv.config();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
+app.set("layout","layout")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -23,13 +25,14 @@ app.use(
 
 app.use("/", storeRouter);
 // app.use("/auth", authRouter);
-// app.use("/products", productRouter);
+app.use("/products", productRouter);
 // app.use("/users", userRouter);
 
 const startServer = async () => {
-    await dbConnect();
-    app.listen(5000, () => {
-  console.log("Server Started");
+  await dbConnect();
+  app.listen(5000, () => {
+    console.log("Server Started");
   });
 };
-startServer();
+
+startServer()
